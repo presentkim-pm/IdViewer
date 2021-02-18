2dd73e222fbb332a2da6f8bb359b5f70853c6453
PresentKim <debe3721@gmail.com>
2021-02-18 21:44:09 +0900

    Init IdViewer plugin

---
+++ src/kim/present/idviewer/IdViewer.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace kim\present\idviewer;
+
+use pocketmine\plugin\PluginBase;
+
+final class IdViewer extends PluginBase{
+    protected function onLoad() : void{
+    }
+
+    protected function onEnable() : void{
+    }
+}
