d1d41389dbefe6f9293a0ec949a3d098638d4cae
PresentKim <debe3721@gmail.com>
2021-02-24 16:17:16 +0900

    Set license to LGPL and add PHP DOC

--- src/kim/present/idviewer/IdViewer.php
+++ src/kim/present/idviewer/IdViewer.php
@@ -1,4 +1,32 @@
 <?php
+
+/**
+ *  ____                           _   _  ___
+ * |  _ \ _ __ ___  ___  ___ _ __ | |_| |/ (_)_ __ ___
+ * | |_) | '__/ _ \/ __|/ _ \ '_ \| __| ' /| | '_ ` _ \
+ * |  __/| | |  __/\__ \  __/ | | | |_| . \| | | | | | |
+ * |_|   |_|  \___||___/\___|_| |_|\__|_|\_\_|_| |_| |_|
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author  PresentKim (debe3721@gmail.com)
+ * @link    https://github.com/PresentKim
+ * @license https://www.gnu.org/licenses/lgpl-3.0 LGPL-3.0 License
+ *
+ *   (\ /)
+ *  ( . .) ♥
+ *  c(")(")
+ *
+ * @noinspection PhpIllegalPsrClassPathInspection
+ * @noinspection PhpDocSignatureInspection
+ * @noinspection PhpDocMissingReturnTagInspection
+ * @noinspection SpellCheckingInspection
+ * @noinspection PhpUnusedParameterInspection
+ */
+
 declare(strict_types=1);
 
 namespace kim\present\idviewer;
@@ -11,6 +39,9 @@ use pocketmine\event\player\PlayerItemHeldEvent;
 use pocketmine\player\Player;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\TextFormat;
+
+use function file_exists;
+use function mkdir;
 
 final class IdViewer extends PluginBase implements Listener{
     protected function onEnable() : void{
